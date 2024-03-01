3cd803555f03f721dc4cb2b94b68217b1a9a09a6
Belfagor2005 <72314956+Belfagor2005@users.noreply.github.com>
2024-03-01 20:06:10 +0100

    Update plugin.py

--- usr/lib/enigma2/python/Plugins/Extensions/freearhey/plugin.py
+++ usr/lib/enigma2/python/Plugins/Extensions/freearhey/plugin.py
@@ -14,14 +14,14 @@ from . import html_conv
 import codecs
 from Components.AVSwitch import AVSwitch
 try:
-    from Components.AVSwitch import iAVSwitch
+    from Components.AVSwitch import iAVSwitch as AVSwitch
 except Exception as e:
     print(e)
     
 try:
-    from enigma import eAVSwitch
+    from enigma import eAVSwitch as AVSwitch
 except Exception:
-    from enigma import eAVControl as eAVSwitch
+    from enigma import eAVControl as AVSwitch
 try:
     from os.path import isdir
 except ImportError:
