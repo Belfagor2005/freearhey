ca856a95c3bd38125f9dcffc2f478b4f7c799070
Belfagor2005 <72314956+Belfagor2005@users.noreply.github.com>
2024-06-30 15:39:00 +0200

    Update Utils.py

--- usr/lib/enigma2/python/Plugins/Extensions/freearhey/Utils.py
+++ usr/lib/enigma2/python/Plugins/Extensions/freearhey/Utils.py
@@ -1424,7 +1424,9 @@ def decodeHtml(text):
         text = text.replace(repl[0], repl[1])
     from re import sub as re_sub
     text = re_sub('<[^>]+>', '', text)
-    return str(text).encode('utf-8').decode('unicode_escape')  # str needed for PLi
+    if PY3:
+        text = text.encode('utf-8').decode('unicode_escape')  
+    return str(text) # str needed for PLi
 
 
 conversion = {
