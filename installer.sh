b86489a095ae82edf32e3d95a1ed00ae962fecc9
Belfagor2005 <72314956+Belfagor2005@users.noreply.github.com>
2024-07-27 16:33:23 +0200

    Update installer.sh

--- installer.sh
+++ installer.sh
@@ -84,7 +84,7 @@ if [ $OSTYPE != "DreamOs" ]; then
 fi
 sleep 2
 
-wget --no-check-certificate --no-cache --no-dns-cache 'https://github.com/Belfagor2005/freearhey/archive/refs/heads/main.tar.gz'
+wget --no-check-certificate 'https://github.com/Belfagor2005/freearhey/archive/refs/heads/main.tar.gz'
 tar -xzf main.tar.gz
 cp -r 'freearhey-main/usr' '/'
 set +e
