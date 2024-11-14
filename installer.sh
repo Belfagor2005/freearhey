582723d846f2af818d6d9edd5ef97dd04315498b
Belfagor2005 <72314956+Belfagor2005@users.noreply.github.com>
2024-11-14 11:21:45 +0100

    Update installer.sh

--- installer.sh
+++ installer.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
-
 ## setup command=wget -q "--no-check-certificate" https://raw.githubusercontent.com/Belfagor2005/freearhey/main/installer.sh -O - | /bin/sh
+exec > >(tee -a /tmp/freearhey_debug.txt) 2>&1
+set -x
 ## Only This 2 lines to edit with new version ######
 version='3.0'
 changelog='\nAdd Live Upgrade\nFix screen'
@@ -111,16 +112,25 @@ if [ ! -d $PLUGINPATH ]; then
 fi
 rm -rf $TMPPATH > /dev/null 2>&1
 sync
-echo ""
-echo ""
-echo "#########################################################"
-echo "#        freearhey Live INSTALLED SUCCESSFULLY          #"
-echo "#                developed by LULULLA                   #"
-echo "#                                                       #"
-echo "#                  https://corvoboys.org                #"
-echo "#########################################################"
-echo "#           your Device will RESTART Now                #"
-echo "#########################################################"
+# # Identify the box type from the hostname file
+FILE="/etc/image-version"
+box_type=$(head -n 1 /etc/hostname)
+distro_value=$(grep '^distro=' "$FILE" | awk -F '=' '{print $2}')
+distro_version=$(grep '^version=' "$FILE" | awk -F '=' '{print $2}')
+python_vers=$(python --version 2>&1)
+echo "#########################################################
+#               INSTALLED SUCCESSFULLY                  #
+#                developed by LULULLA                   #
+#               https://corvoboys.org                   #
+#########################################################
+#           your Device will RESTART Now                #
+#########################################################
+^^^^^^^^^^Debug information:
+BOX MODEL: $box_type
+OO SYSTEM: $OSTYPE
+PYTHON: $python_vers
+IMAGE NAME: $distro_value
+IMAGE VERSION: $distro_version" >> /tmp/freearhey_debug.txt
 sleep 5
 killall -9 enigma2
 exit 0
