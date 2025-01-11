c2bc4776da3f7e53b806ab00838e1a849e79dcdc
Belfagor2005 <72314956+Belfagor2005@users.noreply.github.com>
2025-01-11 21:23:19 +0100

    Update installer.sh

--- installer.sh
+++ installer.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-## setup command=wget -q "--no-check-certificate" https://raw.githubusercontent.com/Belfagor2005/freearhey/main/installer.sh -O - | /bin/sh
+## setup command=wget -q --no-check-certificate https://raw.githubusercontent.com/Belfagor2005/freearhey/main/installer.sh -O - | /bin/sh
 
 ## Only This 2 lines to edit with new version ######
 version='3.0'
