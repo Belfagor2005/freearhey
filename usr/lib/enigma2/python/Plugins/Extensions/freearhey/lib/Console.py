8fb6d437b0503d637e145c05e0c5607eb060480c
Belfagor2005 <72314956+Belfagor2005@users.noreply.github.com>
2025-03-07 20:03:11 +0100

    Update Console.py

--- usr/lib/enigma2/python/Plugins/Extensions/freearhey/lib/Console.py
+++ usr/lib/enigma2/python/Plugins/Extensions/freearhey/lib/Console.py
@@ -1,17 +1,20 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 # RAED & mfaraj57 &  (c) 2018
+# mod Lululla 20240720
 
 from __future__ import print_function
-
+# from . import _
 from enigma import eConsoleAppContainer
 from Screens.Screen import Screen
+from Components.Label import Label
 from Components.ActionMap import ActionMap
 from Components.ScrollLabel import ScrollLabel
-from Components.Sources.StaticText import StaticText
+
 from Screens.MessageBox import MessageBox
 from enigma import getDesktop
 import sys
+
 
 PY2 = sys.version_info[0] == 2
 PY3 = sys.version_info[0] == 3
@@ -60,26 +63,29 @@ class Console(Screen):
             self.skinName = [skin, 'Console']
         self.errorOcurred = False
         self['text'] = ScrollLabel('')
-        self['key_red'] = StaticText(_('Cancel'))
-        self['key_green'] = StaticText(_('Hide'))
+        self['key_red'] = Label(_('Cancel'))
+        self['key_green'] = Label(_('Hide/Show'))
+        self['key_blue'] = Label(_('Restart'))
+
         self["actions"] = ActionMap(["WizardActions", "DirectionActions", 'ColorActions'],
                                     {
-                                        "ok": self.cancel if not self.finished else None,
-                                        "up": self["text"].pageUp,
-                                        "down": self["text"].pageDown,
-                                        "red": self.cancel,
-                                        "green": self.toggleHideShow,
-                                        "blue": self.restartenigma,
-                                        "exit": self.cancel,
+                                    "ok": self.cancel,
+                                    "up": self["text"].pageUp,
+                                    "down": self["text"].pageDown,
+                                    "red": self.cancel,
+                                    "green": self.toggleHideShow,
+                                    "blue": self.restartenigma,
+                                    "exit": self.cancel,
                                     }, -1)
-        self.cmdlist = isinstance(cmdlist, list) and cmdlist or [cmdlist]
+
         self.newtitle = title == 'Console' and _('Console') or title
+        self.cmdlist = isinstance(cmdlist, list) and cmdlist or [cmdlist]
         self.cancel_msg = None
         self.onShown.append(self.updateTitle)
         self.container = eConsoleAppContainer()
         self.run = 0
         self.finished = False
-        try:  # DreamOS By RAED
+        try:
             self.container.appClosed.append(self.runFinished)
             self.container.dataAvail.append(self.dataAvail)
         except:
@@ -92,13 +98,12 @@ class Console(Screen):
 
     def startRun(self):
         if self.showStartStopText:
-            self['text'].setText(_('Execution progress') + '\n\n')
-        print('[Console] executing in run', self.run, 'the command:', self.cmdlist[self.run])
+            self['text'].setText(_('Execution progress\n\n'))
+        print('[Console] executing in run', self.run, ' the command:', self.cmdlist[self.run])
+        print("[Console] Executing command:", self.cmdlist[self.run])  # Aggiungi questo print
         if self.container.execute(self.cmdlist[self.run]):
-            print('[Console] Command executed successfully')
+            self['text'].setText(self.cmdlist[self.run])
             self.runFinished(-1)
-        else:
-            print('[Console] Command failed to execute')
 
     def runFinished(self, retval):
         if retval:
@@ -111,20 +116,20 @@ class Console(Screen):
         else:
             self.show()
             self.finished = True
-            try:
-                lastpage = self['text'].isAtLastPage()
-            except:
-                lastpage = self['text']
+            # try:
+                # lastpage = self['text'].isAtLastPage()
+            # except:
+                # lastpage = self['text']
             if self.cancel_msg:
                 self.cancel_msg.close()
             if self.showStartStopText:
-                self['text'].appendText(_('Execution finished!!'))
+                self['text'].appendText('Execution finished!!')
             if self.finishedCallback is not None:
                 self.finishedCallback()
             if not self.errorOcurred and self.closeOnSuccess:
                 self.closeConsole()
             else:
-                self['text'].appendText(_('\nPress OK or Exit to abort!'))
+                self['text'].appendText('\nPress OK or Exit to abort!')
                 self['key_red'].setText(_('Exit'))
                 self['key_green'].setText('')
 
@@ -140,12 +145,12 @@ class Console(Screen):
         if self.finished:
             self.closeConsole()
         else:
-            self.cancel_msg = self.session.openWithCallback(self.cancelCallback, MessageBox, _('Cancel execution?'), type=MessageBox.TYPE_YESNO, default=False)
+            self.cancel_msg = self.session.openWithCallback(self.cancelCallback, MessageBox, 'Cancel execution?', type=MessageBox.TYPE_YESNO, default=False)
 
     def cancelCallback(self, ret=None):
         self.cancel_msg = None
         if ret:
-            try:  # DreamOS By RAED
+            try:
                 self.container.appClosed.remove(self.runFinished)
                 self.container.dataAvail.remove(self.dataAvail)
             except:
@@ -156,7 +161,7 @@ class Console(Screen):
 
     def closeConsole(self):
         if self.finished:
-            try:  # DreamOS By RAED
+            try:
                 self.container.appClosed.remove(self.runFinished)
                 self.container.dataAvail.remove(self.dataAvail)
             except:
@@ -167,11 +172,12 @@ class Console(Screen):
             self.show()
 
     def dataAvail(self, str):
-        print("Received data:", str)  # Aggiungi un print per vedere cosa ricevi
         if PY3:
-            self['text'].appendText(str.decode())
+            data = str.decode()
         else:
-            self['text'].appendText(str)
+            data = str
+        print("[Console] Data received: ", data)
+        self['text'].appendText(data)
 
     def restartenigma(self):
         from Screens.Standby import TryQuitMainloop
